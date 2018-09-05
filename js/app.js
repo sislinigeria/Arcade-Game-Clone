698aea1ba916a5530ed69efd86a0f128a7be945c
Sixtus Nnamdi <sixtus.nnamdi@gmail.com>
2018-09-05 09:11:29 +0100

    correct typo in L17 JS file

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var Enemy = function (x, y, speed) {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter within the x axis
-    this.x =+ this.speed * dt;
+    this.x += this.speed * dt;
     // which will ensure the game runs at the same speed for
     // all computers.
 
